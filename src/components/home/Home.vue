5f091a45b5ee68634471ccb98f20bfb3f2241cc6
Guilherme Wollmann <gwdoliveira@inf.ufpel.edu.br>
2019-10-03 16:08:12 -0300

    versao final

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,19 +3,26 @@
     <PageTitle icon="fa fa-home" main="Home" sub="Lista de Incidencias" />
     <template>
       <div>
-        <b-table striped hover :items="entries" :fields="fields"></b-table> 
+        <b-table striped hover :items="entries" :fields="fields"></b-table>
       </div>
     </template>
+    <div class="load-more">
+      <button
+        v-if="loadMore"
+        class="btn btn-lg btn-outline-primary"
+        @click="loadEntries"
+      >Carregar Mais Entradas</button>
+      <span v-else>Voce não possui novas entradas.</span>
+    </div>
   </div>
 </template>
 
 
 <script>
-import { baseApiUrl } from '@/global'
-import axios from 'axios'
+import { baseApiUrl } from "@/global";
+import axios from "axios";
 import { mapState } from "vuex";
 import PageTitle from "../template/PageTitle";
-
 
 export default {
   name: "Home",
@@ -23,31 +30,43 @@ export default {
   computed: mapState(["user"]),
   data: function() {
     return {
+      page: 1,
+      loadMore: true,
       entries: [],
       fields: [
         { key: "id", label: "Entrada", sortable: true },
         { key: "typeof", label: "Tipo", sortable: true },
-        { key: "createdat", label: "Data", sortable: true,
-        formatter: (key) =>{
-            var parts = key.split('T')
-            return parts[0] 
-      }
-  }]
-  }},
+        {key: "createdat",label: "Data",sortable: true,
+          formatter: key => {
+            var parts = key.split("T");
+            return parts[0];
+          }
+        }
+      ]
+    };
+  },
   methods: {
     loadEntries() {
-      const url = `${baseApiUrl}/entries/${this.user.id}`
+      const url = `${baseApiUrl}/entries/${this.user.id}?page=${this.page}`;
       axios.get(url).then(res => {
-      this.entries = res.data
-      })
+        this.entries = this.entries.concat(res.data);
+
+        this.page++;
+        if (res.data.length === 0) this.loadMore = false;
+      });
     }
   },
   mounted() {
     this.loadEntries();
   }
-}
-
+};
 </script>
 
 <style>
+.load-more {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 25px;
+}
 </style>
