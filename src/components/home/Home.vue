920b6582b811aa7cd58c6c968b19c00bfb1116d5
Guilherme Wollmann <gwdoliveira@inf.ufpel.edu.br>
2019-11-17 20:08:48 -0300

    Versão Final

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -14,6 +14,12 @@
       >Carregar Mais Entradas</button>
       <span v-else>Voce não possui novas entradas.</span>
     </div>
+    <br>
+    <template>
+      <div>
+        <b-table striped hover :items="values" :fields="types"></b-table>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -32,9 +38,16 @@ export default {
     return {
       page: 1,
       loadMore: true,
+      types: [
+        {key:"typeof", label: "Tipo"},
+        {key:"db", label: "Valor em Db"},
+        ],
+      values: [{typeof:"Alerta 1",db:"De 80 a 90 DB's"},
+      {typeof:"Alerta 2",db:"De 90 a 100 DB's"},
+      {typeof:"Alerta 3",db:"100 ou mais DB's"}],
       entries: [],
       fields: [
-        { key: "id", label: "Entrada", sortable: true },
+        { key: "id", label: "Entrada", sortable: true},
         { key: "typeof", label: "Tipo", sortable: true },
         {key: "createdat",label: "Data",sortable: true,
           formatter: key => {
@@ -47,9 +60,15 @@ export default {
   },
   methods: {
     loadEntries() {
+      var entryCount = 1;
       const url = `${baseApiUrl}/entries/${this.user.id}?page=${this.page}`;
       axios.get(url).then(res => {
         this.entries = this.entries.concat(res.data);
+
+       
+         this.entries.forEach(element => {
+           element.id = entryCount++;
+         });
 
         this.page++;
         if (res.data.length === 0) this.loadMore = false;
